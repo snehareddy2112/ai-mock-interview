d9f2b289507e5f28a56d685587d445ae5e57dd53
snehareddy2112 <snehareddyaelijerla@gmail.com>
2026-03-01 18:34:42 +0530

    Added test user floating card

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-// Automatically attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN FROM STORAGE:", token); // Debug
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
